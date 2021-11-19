112d86ba0ad407c883dbfa46818e92f4b74ed044
Gabrielly Oliveira <93413622+OliveiraGabsMaria@users.noreply.github.com>
2021-11-19 11:38:55 -0300

    Projeto Bloco I

--- Projeto Bloco I - ModaxGen/ModasXGen/EnderecoModaXGen.java
+++ Projeto Bloco I - ModaxGen/ModasXGen/EnderecoModaXGen.java
@@ -5,7 +5,7 @@ public class EnderecoModaXGen {
 		String enderecofim;
 			
 			EnderecoModaXGen(String n){
-				//System.out.println("Inicializando o construtor da Classe..");
+				//System.out.println("Inicializando o construtor da Classe...");
 				enderecofim = n;
 		}
 }
